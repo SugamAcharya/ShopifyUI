ee8fd63eec70211ad8e3ff92f2229c9a22e49c1d
sugam12 <sugamachr@gmail.com>
2025-05-10 10:49:16 +0800

    Initial commit for VueJS setting

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <router-link to="/">Test HOme</router-link>
+  </div>
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+$primary: #6f42c1;
+@import 'node_modules/bootstrap/scss/bootstrap';
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1 class="text-primary"><fa icon="shopping-cart"></fa>Shopping Cart</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
